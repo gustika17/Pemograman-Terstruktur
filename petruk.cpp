213e1fa1080026c6293e364492553c6594aac8ce
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-04-13 10:20:40 +0700

    Add files via upload

---
+++ petruk.cpp
@@ -0,0 +1,22 @@
+//Gustika Dwi Mardini
+//2257051004
+//C
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main(){
+	string teks[4];
+	
+	for(int i=0; i<4; i++){
+		cin>>teks[i];
+	}
+	
+	ofstream readFile("barang.txt");
+	
+	for(int i=0; i<4; i++){
+		readFile<<teks[i]<<"\n";
+	}
+	return 0;
+}
